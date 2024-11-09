73ca26e06368631ff86f24921645e97c651a3925
Sukhrob Abdullaev <s.abdullaev@tuit.uz>
2024-11-09 13:22:33 +0900

    add day8 with exercises of caesar cipher algorithm

---
+++ projects/day8/ceaser1.py
@@ -0,0 +1,34 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+print(20%26)
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
+
+# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
+#  by the shift amount and print the encrypted text.
+
+# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
+
+# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
+#  message.
+
+
+def encrypt(original_text, shift_amount):
+    cipher_text = ""
+    for letter in original_text:
+        # sukhrob
+        shifted_position = alphabet.index(letter) + shift_amount
+        # = index(z)+2 => 26+2=28
+        # print(shifted_position)
+        shifted_position %= len(alphabet)
+        # =28%26 -> 2  
+        cipher_text += alphabet[shifted_position]
+    print(f"Here is the encoded result: {cipher_text}")
+
+
+encrypt(original_text=text, shift_amount=shift)
+
+# ucnqo
---
+++ projects/day8/positional.py
@@ -0,0 +1,9 @@
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+# Positional arguments
+# greet_with("Jack Bauer", "Nowhere")
+# greet_with("Nowhere", "Jack Bauer")
+
+# Keyword arguments
+greet_with(location="London", name="Angela")
