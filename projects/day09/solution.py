39d841c106147e6aa2734879ce8bc2d26b95ea26
Sukhrob Abdullaev <s.abdullaev@tuit.uz>
2024-11-26 17:56:55 +0900

    day10

--- projects/day09/solution.py
+++ projects/day09/solution.py
@@ -18,9 +18,11 @@ def find_highest_bidder(bidding_record):
 bids={}
 continue_bidding=True
 while continue_bidding:
+
 	name=input("What is your name?")
 	price =int(input("What is your bid?: $"))
 	bids[name] = price
+	
 	should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
 	if should_continue == "no":
 			continue_bidding = False
